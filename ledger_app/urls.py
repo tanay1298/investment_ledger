9de8fa98a7f8ccb35f5a6caad8ddcc5c43f3c302
Tanay Burreja <tanay.burrej12@gmail.com>
2024-01-19 03:15:00 +0530

    upgrade django

--- ledger_app/urls.py
+++ ledger_app/urls.py
@@ -1,7 +1,6 @@
-# ledger_app/urls.py
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ledger_view, name='ledger'),
+    path('', views.ledger_view, name='ledger'),
 ]
